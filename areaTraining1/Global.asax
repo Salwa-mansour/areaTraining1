19c3be0930ca59cdae8db4c61d0924d50a334931
Salwa-mansour <alqadasisal@gmail.com>
2022-08-06 09:50:45 +0300

    Add project files.

---
+++ areaTraining1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="areaTraining1.MvcApplication" Language="C#" %>
---
+++ areaTraining1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(areaTraining1.Startup))]
+namespace areaTraining1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
